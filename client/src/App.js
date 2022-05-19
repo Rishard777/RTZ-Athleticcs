56a8d88281c9ab5e3e4bc374371bc8efcada793b
Rishard Davis <rrdavis777@gmail.com>
2022-05-18 21:11:12 -0400

    Navbar setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from "react";
-import logo from './logo.svg';
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from './Login';
+import Home from './Home';
+import NavBar from './NavBar';
 import './App.css';
+
+
 
 function App() {
   const [user, setUser] = useState(null);
@@ -9,18 +14,40 @@ function App() {
   function handleLogin(user) {
     setUser(user);
   }
-  if(user){
-    return (
-      <div className="App">
-        Welcome!
-      </div>
-    )
-  } else
+
   return (
-    <div className="App">
-      <Login  onLogin={handleLogin} />
+<div>
+    <NavBar />
+    
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login">
+        <Login onLogin={handleLogin} />
+      </Route>
+
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+
+    </BrowserRouter>
     </div>
-  );
-}
+
+  // if(user){
+  //   return (
+  //     <div className="App">
+  //       Welcome!
+  //     </div>
+  //   )
+  // } else
+  // return (
+  //   <div className="App">
+  //     <Login  onLogin={handleLogin} />
+  //   </div>
+  // );
+)}
+  
+
+
 
 export default App;
